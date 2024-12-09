a14c2bf9f60b4107944d837eec39d47b840c4eca
Tho Nguyen Xuan <xuantho573@gmail.com>
2024-12-09 22:44:26 +0700

    nvim(telescope): update keymap for finding files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -24,9 +24,12 @@ return {
     telescope.load_extension("fzf")
 
     -- set keymaps
-    -- local keymap = vim.keymap -- for conciseness
+    require("which-key").add({
+      mode = { "n" },
+      { "<leader>ff", "<cmd>Telescope find_files find_command=fd,-H<cr>", desc = "Find Files" },
+    })
 
-    -- keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    -- keymap.set("n", "<leader>ff", "<cmd>Telescope find_files find_command=fd,-H<cr>", { desc = "Find files" })
     -- keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     -- keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     -- keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
