4154826523a90e8ff1b200e105e22b9e8c181404
Tho Nguyen Xuan <xuantho573@gmail.com>
2024-12-09 22:45:20 +0700

    nvim(mason): setup auto installer

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,16 +3,18 @@ return {
     "williamboman/mason.nvim",
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
     lazy = true,
     config = function()
       local mason = require("mason")
       local mason_lspconfig = require("mason-lspconfig")
+      local mason_tool_installer = require("mason-tool-installer")
 
       mason.setup({
         ui = {
           border = "rounded",
-          width = 0.9,
+          width = 0.8,
           height = 0.8,
           icons = {
             package_installed = "✓",
@@ -25,6 +27,15 @@ return {
       mason_lspconfig.setup({
         ensure_installed = {
           "vtsls",
+          -- "volar",
+          "vuels",
+          "tailwindcss",
+        },
+      })
+
+      mason_tool_installer.setup({
+        ensure_installed = {
+          "eslint_d",
         },
       })
     end,
