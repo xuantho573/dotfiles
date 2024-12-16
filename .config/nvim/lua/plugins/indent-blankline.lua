f003fe8da7d580ff682fed57be5d828b85b490cb
Tho Nguyen Xuan <xuantho573@gmail.com>
2024-12-16 20:05:13 +0700

    nvim(indent-blankline): use catppuccin's palettes provided via package

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,35 +1,3 @@
-local highlight = {}
-
-local ctp_mocha_mappings = {
-  { name = "Rosewater", color = "#f4dbd6" },
-  { name = "Flamingo", color = "#f0c6c6" },
-  { name = "Pink", color = "#f5bde6" },
-  { name = "Mauve", color = "#c6a0f6" },
-  { name = "Red", color = "#ed8796" },
-  { name = "Maroon", color = "#ee99a0" },
-  { name = "Peach", color = "#f5a97f" },
-  { name = "Yellow", color = "#eed49f" },
-  { name = "Green", color = "#a6da95" },
-  { name = "Teal", color = "#8bd5ca" },
-  { name = "Sky", color = "#91d7e3" },
-  { name = "Sapphire", color = "#7dc4e4" },
-  { name = "Blue", color = "#8aadf4" },
-  { name = "Lavender", color = "#b7bdf8" },
-}
-
-for idx, val in ipairs(ctp_mocha_mappings) do
-  highlight[idx] = "Rainbow" .. val.name
-end
-
-local hooks = require("ibl.hooks")
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  for _, val in ipairs(ctp_mocha_mappings) do
-    vim.api.nvim_set_hl(0, "Rainbow" .. val.name, { fg = val.color })
-  end
-end)
-
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
@@ -37,8 +5,49 @@ return {
   ---@type ibl.config
   opts = {},
   config = function()
+    local palette
+    if vim.o.bg == "light" then
+      palette = require("catppuccin.palettes").get_palette("latte")
+    else
+      palette = require("catppuccin.palettes").get_palette("macchiato")
+    end
+
+    function check_if_accent(name)
+      return not (
+        string.find(name, "text")
+        or string.find(name, "sur")
+        or string.find(name, "over")
+        or name == "base"
+        or name == "mantle"
+        or name == "crust"
+      )
+    end
+
+    function get_highlight_name(name)
+      return "Rainbow" .. name:gsub("^%l", string.upper)
+    end
+
+    local highlight = {}
+    local idx = 1
+    for name, hex in pairs(palette) do
+      if check_if_accent(name) then
+        highlight[idx] = get_highlight_name(name)
+        idx = idx + 1
+      end
+    end
+
+    local hooks = require("ibl.hooks")
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      for name, hex in pairs(palette) do
+        if check_if_accent(name) then
+          vim.api.nvim_set_hl(0, get_highlight_name(name), { fg = hex })
+        end
+      end
+    end)
+
     require("ibl").setup({
       indent = { highlight = highlight, char = "│" },
+      scope = { enabled = false, char = "┃" },
     })
   end,
 }
