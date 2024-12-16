59a8660902a135537c5493b5355780bb9c11cbc8
Tho Nguyen Xuan <xuantho573@gmail.com>
2024-12-16 20:37:23 +0700

    nvim(lazygit): update lazygit UI config

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -17,4 +17,19 @@ return {
   keys = {
     { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
   },
+  config = function()
+    vim.g.lazygit_floating_window_winblend = 1
+    vim.g.lazygit_use_custom_config_file_path = 1
+    if vim.o.bg == "light" then
+      vim.g.lazygit_config_file_path = {
+        "/home/xuantho573/.config/lazygit/config.yml",
+        "/home/xuantho573/.config/lazygit/catppuccin-latte-sky.yml",
+      }
+    else
+      vim.g.lazygit_config_file_path = {
+        "/home/xuantho573/.config/lazygit/config.yml",
+        "/home/xuantho573/.config/lazygit/catppuccin-macchiato-sky.yml",
+      }
+    end
+  end,
 }
