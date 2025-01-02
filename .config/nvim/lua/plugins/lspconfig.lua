4ecbad8148f04a644824cbc12f613e580b8fbd44
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-01-02 13:51:13 +0700

    nvim(lspconfig): disable auto formating from lsp

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -22,16 +22,17 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     local on_attach = function(client, bufnr)
-      if client.server_capabilities.documentFormattingProvider then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = vim.api.nvim_create_augroup("Format", { clear = true }),
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format()
-          end,
-        })
-      end
+      -- if client.server_capabilities.documentFormattingProvider then
+      --   vim.api.nvim_create_autocmd("BufWritePre", {
+      --     group = vim.api.nvim_create_augroup("Format", { clear = true }),
+      --     buffer = bufnr,
+      --     callback = function()
+      --       vim.lsp.buf.format()
+      --     end,
+      --   })
+      -- end
     end
+    -- keymap.set("n", "<leader>fx", "mF:%!eslint_d --stdin --fix-to-stdout --stdin-filename %<CR>`F")
 
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
