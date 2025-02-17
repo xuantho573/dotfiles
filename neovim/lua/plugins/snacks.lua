d048491c50603ffe0f9343c3ab3ba6abd1f8ea46
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-02-17 08:27:18 +0700

    chore(neovim/snacks): restructure config files

--- neovim/lua/plugins/snacks.lua
+++ neovim/lua/plugins/snacks.lua
@@ -3,29 +3,6 @@ return {
   version = "2.20.0",
   ---@type snacks.Config
   opts = {
-    ---@class snacks.lazygit.Config: snacks.terminal.Opts
-    ---@field args? string[]
-    ---@field theme? snacks.lazygit.Theme
-    lazygit = {
-      configure = true,
-      config = {
-        os = { editPreset = "nvim-remote" },
-        gui = { nerdFontsVersion = "3" },
-      },
-      theme_path = vim.fs.normalize(vim.fn.stdpath("cache") .. "/lazygit-theme.yml"),
-      theme = {
-        [241]                      = { fg = "Special" },
-        activeBorderColor          = { fg = "CatppuccinFgSky", bold = true },
-        cherryPickedCommitBgColor  = { fg = "CatppuccinFgSurface1" },
-        cherryPickedCommitFgColor  = { fg = "CatppuccinFgSky" },
-        defaultFgColor             = { fg = "CatppuccinFgText" },
-        inactiveBorderColor        = { fg = "CatppuccinFgSubtext0" },
-        optionsTextColor           = { fg = "CatppuccinFgBlue" },
-        searchingActiveBorderColor = { fg = "CatppuccinFgYellow" },
-        selectedLineBgColor        = { bg = "CatppuccinBgSurface0" },
-        unstagedChangesColor       = { fg = "CatppuccinFgRed" },
-      },
-      win = { style = "lazygit" },
-    },
+    lazygit = require("plugins.snacks.lazygit")
   },
 }
---
+++ neovim/lua/plugins/snacks/lazygit.lua
@@ -0,0 +1,24 @@
+---@class snacks.lazygit.Config: snacks.terminal.Opts
+---@field args? string[]
+---@field theme? snacks.lazygit.Theme
+return {
+  configure = true,
+  config = {
+    os = { editPreset = "nvim-remote" },
+    gui = { nerdFontsVersion = "3" },
+  },
+  theme_path = vim.fs.normalize(vim.fn.stdpath("cache") .. "/lazygit-theme.yml"),
+  theme = {
+    [241]                      = { fg = "Special" },
+    activeBorderColor          = { fg = "CatppuccinFgSky", bold = true },
+    cherryPickedCommitBgColor  = { fg = "CatppuccinFgSurface1" },
+    cherryPickedCommitFgColor  = { fg = "CatppuccinFgSky" },
+    defaultFgColor             = { fg = "CatppuccinFgText" },
+    inactiveBorderColor        = { fg = "CatppuccinFgSubtext0" },
+    optionsTextColor           = { fg = "CatppuccinFgBlue" },
+    searchingActiveBorderColor = { fg = "CatppuccinFgYellow" },
+    selectedLineBgColor        = { bg = "CatppuccinBgSurface0" },
+    unstagedChangesColor       = { fg = "CatppuccinFgRed" },
+  },
+  win = { style = "lazygit" },
+}
