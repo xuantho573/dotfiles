de3af84f7af122481adcdcd01bd8e7a7442d3ff0
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-02-17 08:14:15 +0700

    chore(neovim): fix LazyVim version

--- neovim/lua/config/lazy.lua
+++ neovim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", version = "13.5.3" },
     -- import/override with your plugins
     { import = "plugins" },
   },
