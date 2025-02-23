217e0c309d3e12313e1162358027318fab18d2b4
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-02-23 17:01:49 +0700

    chore(neovim/nvim-tree): restructure config file and fix version

--- neovim/lua/plugins/nvim-tree.lua
+++ neovim/lua/plugins/nvim-tree.lua
@@ -1,98 +1,59 @@
+local WIDTH_RATIO = 0.3
+local HEIGHT_RATIO = 0.9
+
 return {
   "nvim-tree/nvim-tree.lua",
+  version = "1.11.0",
   dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    local nvimtree = require("nvim-tree")
-
-    local HEIGHT_RATIO = 0.9
-    local WIDTH_RATIO = 0.3
-
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
-    nvimtree.setup({
-      view = {
-        width = function()
-          return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+  keys = {
+    { "<leader>ee", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file explorer", mode = "n" },
+    { "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", desc = "Toggle file explorer on current file", mode = "n" },
+    { "<leader>ec", "<cmd>NvimTreeCollapse<CR>", desc = "Collapse file explorer", mode = "n" },
+    { "<leader>er", "<cmd>NvimTreeRefresh<CR>", desc = "Refresh file explorer", mode = "n" },
+  },
+  opts = {
+    view = {
+      width = function()
+        return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+      end,
+      float = {
+        enable = true,
+        open_win_config = function()
+          local window_w = vim.opt.columns:get() * WIDTH_RATIO
+          local window_h = (vim.opt.lines:get() - vim.opt.cmdheight:get()) * HEIGHT_RATIO
+          local window_w_int = math.floor(window_w)
+          local window_h_int = math.max(math.floor(window_h), 20)
+          local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+          return {
+            border = "rounded",
+            relative = "editor",
+            row = center_y,
+            col = 0,
+            width = window_w_int,
+            height = window_h_int,
+          }
         end,
-        float = {
-          enable = true,
-          open_win_config = function()
-            local screen_w = vim.opt.columns:get()
-            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-            local window_w = screen_w * WIDTH_RATIO
-            local window_h = screen_h * HEIGHT_RATIO
-            local window_w_int = math.floor(window_w)
-            local window_h_int = math.max(math.floor(window_h), 20)
-            local center_x = (screen_w - window_w) / 2
-            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
-            return {
-              border = "rounded",
-              relative = "editor",
-              row = center_y,
-              -- col = center_x,
-              col = 0,
-              width = window_w_int,
-              height = window_h_int,
-            }
-          end,
+      },
+    },
+    renderer = {
+      indent_markers = { enable = true },
+      icons = {
+        git_placement = "after",
+        show = {
+          folder_arrow = false,
+          git = true,
         },
       },
-      -- change folder arrow icons
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          --[[
-          glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
-            },
-          },
-          ]]
-          git_placement = "after",
-          show = {
-            folder_arrow = false,
-            git = true,
-          },
-        },
+    },
+    -- disable window_picker for explorer to work well with window splits
+    actions = {
+      open_file = {
+        window_picker = { enable = false },
       },
-      -- disable window_picker for
-      -- explorer to work well with
-      -- window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      filters = {
-        custom = { ".DS_Store" },
-      },
-      git = {
-        ignore = false,
-      },
-    })
-
-    -- set keymaps
-    -- local keymap = vim.keymap -- for conciseness
-
-    -- keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    -- keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    -- keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    -- keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-
-    require("which-key").add({
-      mode = { "n" },
-      { "<leader>e", group = "explorer", icon = { icon = "󰙅", color = "green" } },
-      { "<leader>ee", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file explorer" },
-      { "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", desc = "Toggle file explorer on current file" },
-      { "<leader>ec", "<cmd>NvimTreeCollapse<CR>", desc = "Collapse file explorer" },
-      { "<leader>er", "<cmd>NvimTreeRefresh<CR>", desc = "Refresh file explorer" },
-    })
-  end,
+    },
+    filters = {
+      custom = { ".DS_Store" },
+    },
+    git = { ignore = false },
+  },
 }
