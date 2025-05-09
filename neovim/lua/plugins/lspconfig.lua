08baf96ef134d9e47e4e0aa13db81fa60c43f5e5
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-05-09 16:29:30 +0700

    chore(neovim/nvim-lspconfig): bump to version 2.1.0 and remove redundant code

--- neovim/lua/plugins/lspconfig.lua
+++ neovim/lua/plugins/lspconfig.lua
@@ -1,8 +1,9 @@
 return {
   "neovim/nvim-lspconfig",
+  version = "2.1.0",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
+    { "hrsh7th/cmp-nvim-lsp", commit = "a8912b8" },
     { "antosha417/nvim-lsp-file-operations", config = true },
     -- { "folke/neodev.nvim", opts = {} },
   },
@@ -21,19 +22,6 @@ return {
 
     local keymap = vim.keymap -- for conciseness
 
-    local on_attach = function(client, bufnr)
-      -- if client.server_capabilities.documentFormattingProvider then
-      --   vim.api.nvim_create_autocmd("BufWritePre", {
-      --     group = vim.api.nvim_create_augroup("Format", { clear = true }),
-      --     buffer = bufnr,
-      --     callback = function()
-      --       vim.lsp.buf.format()
-      --     end,
-      --   })
-      -- end
-    end
-    -- keymap.set("n", "<leader>fx", "mF:%!eslint_d --stdin --fix-to-stdout --stdin-filename %<CR>`F")
-
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
       callback = function(ev)
@@ -42,29 +30,12 @@ return {
         local opts = { buffer = ev.buf, silent = true }
 
         -- set keybinds
-        opts.desc = "Show LSP references"
-        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-        opts.desc = "Go to declaration"
-        keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-        opts.desc = "Show LSP definitions"
-        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-        opts.desc = "Show LSP implementations"
-        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-        opts.desc = "Show LSP type definitions"
-        keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
         opts.desc = "See available code actions"
         keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
         opts.desc = "Smart rename"
         keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-        opts.desc = "Show buffer diagnostics"
-        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
         opts.desc = "Show line diagnostics"
         keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
@@ -103,7 +74,6 @@ return {
       function(server_name)
         lspconfig[server_name].setup({
           capabilities = capabilities,
-          on_attach = on_attach,
         })
       end,
 
@@ -129,7 +99,6 @@ return {
         -- configure typescript server (with special settings)
         lspconfig["ts_ls"].setup({
           capabilities = capabilities,
-          on_attach = on_attach,
           init_options = {
             plugins = {
               {
@@ -143,15 +112,5 @@ return {
         })
       end,
     })
-
-    -- lspconfig["ts_ls"].setup({})
-    -- lspconfig["vtsls"].setup({
-    --   capabilities = capabilities,
-    --   settings = {
-    --     js = {
-    --       implicitProjectConfig = { checkJs = true },
-    --     },
-    --   },
-    -- })
   end,
 }
