66a8092d497e2d459dd3e80c16ebdd53863afc36
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-05-09 15:33:15 +0700

    feat(neovim): replace telescope.nvim with snacks picker

--- neovim/lua/plugins/snacks.lua
+++ neovim/lua/plugins/snacks.lua
@@ -1,9 +1,11 @@
 local dashboard = require("plugins.snacks.dashboard")
 local lazygit = require("plugins.snacks.lazygit")
+local picker = require('plugins.snacks.picker')
 
 return {
   "folke/snacks.nvim",
   version = "2.20.0",
+  keys = picker.keys,
   ---@type snacks.Config
   opts = {
     dashboard = dashboard.opts,
