32ac2c56b5587940a12dad8edc52659ec49ac969
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-05-09 15:28:52 +0700

    chore(neovim/snacks): restructure

--- neovim/lua/plugins/snacks.lua
+++ neovim/lua/plugins/snacks.lua
@@ -1,9 +1,12 @@
+local dashboard = require("plugins.snacks.dashboard")
+local lazygit = require("plugins.snacks.lazygit")
+
 return {
   "folke/snacks.nvim",
   version = "2.20.0",
   ---@type snacks.Config
   opts = {
-    dashboard = require("plugins.snacks.dashboard"),
-    lazygit = require("plugins.snacks.lazygit"),
+    dashboard = dashboard.opts,
+    lazygit = lazygit.opts,
   },
 }
