8237be7bd74ee9fe8f6da0481a45793163a75788
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-05-09 22:27:34 +0700

    chore(neovim/nvim-cmp): bump to version 0.0.2 and remove redundant code

--- neovim/lua/plugins/nvim-cmp.lua
+++ neovim/lua/plugins/nvim-cmp.lua
@@ -1,20 +1,14 @@
 return {
   "hrsh7th/nvim-cmp",
+  version = "0.0.2",
   event = "InsertEnter",
   dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
-    "hrsh7th/cmp-path", -- source for file system paths
+    { "hrsh7th/cmp-buffer", commit = "3022dbc" }, -- source for text in buffer
+    { "hrsh7th/cmp-path", commit = "91ff86c" }, -- source for file system paths
     -- "onsails/lspkind.nvim", -- vs-code like pictograms
   },
   config = function()
     local cmp = require("cmp")
-
-    -- local luasnip = require("luasnip")
-
-    -- local lspkind = require("lspkind")
-
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    -- require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
       window = {
@@ -24,13 +18,6 @@ return {
       completion = {
         completeopt = "menu,menuone,preview,noselect",
       },
-      --[[
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      ]]
       mapping = cmp.mapping.preset.insert({
         ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
         ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
@@ -47,16 +34,6 @@ return {
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
       }),
-
-      -- configure lspkind for vs-code like pictograms in completion menu
-      --[[
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-        }),
-      },
-      ]]
     })
   end,
 }
