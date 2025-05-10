85d8532f8a350feee65b4b360c220dea143be782
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-05-10 07:31:31 +0700

    chore(neovim/nvim-treesitter): add config for nvim-treesitter-textobjects

--- neovim/lua/plugins/nvim-treesitter.lua
+++ neovim/lua/plugins/nvim-treesitter.lua
@@ -1,27 +1,34 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  commit = "28d480e",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "lua",
-      "javascript",
-      "typescript",
-      "tsx",
-      "json",
-      "yaml",
-      "http",
-      "html",
-      "css",
-      "scss",
-      "vue",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    commit = "28d480e",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        "yaml",
+        "http",
+        "html",
+        "css",
+        "scss",
+        "vue",
+      },
+      ignore_install = {},
+      highlight = {
+        enable = true,
+      },
     },
-    ignore_install = {},
-    highlight = {
-      enable = true,
-    },
+    -- config = function()
+    --   local treesitter = require("nvim-treesitter")
+    -- end,
   },
-  -- config = function()
-  --   local treesitter = require("nvim-treesitter")
-  -- end,
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    commit = "0e3be38",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+  }
 }
