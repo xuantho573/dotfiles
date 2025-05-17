d2df909d9bee38b4baf16d11f3668e4b7d5d537b
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-05-17 20:38:50 +0700

    chore(neovim/snacks): refactor for easier modules extend

--- neovim/lua/plugins/snacks.lua
+++ neovim/lua/plugins/snacks.lua
@@ -1,17 +1,33 @@
 local dashboard = require("plugins.snacks.dashboard")
-local indent = require('plugins.snacks.indent')
+local indent = require("plugins.snacks.indent")
 local lazygit = require("plugins.snacks.lazygit")
-local picker = require('plugins.snacks.picker')
+local picker = require("plugins.snacks.picker")
+
+local modules = {
+  dashboard = dashboard,
+  indent = indent,
+  lazygit = lazygit,
+  picker = picker,
   statuscolumn = { opts = {} },
+}
+
+local function get_keys(_, module)
+  return module.keys
+end
+
+local function get_opts(module_name, module)
+  return { [module_name] = module.opts }
+end
+
+local function merge_opts(...)
+  return vim.tbl_extend("force", ...)
+end
 
 return {
   "folke/snacks.nvim",
   version = "2.20.0",
-  keys = picker.keys,
-  ---@type snacks.Config
-  opts = {
-    dashboard = dashboard.opts,
-    indent = indent.opts,
-    lazygit = lazygit.opts,
-  },
+  priority = 1000,
+  lazy = false,
+  keys = vim.iter(vim.iter(modules):map(get_keys):totable()):flatten():totable(),
+  opts = vim.iter(modules):map(get_opts):fold({}, merge_opts),
 }
