16beb7dd23de30a99495167cacfe5b2684b00759
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-05-17 22:36:31 +0700

    chore(neovim/nvim-ts-autotag): add config file

---
+++ neovim/lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,11 @@
+return {
+  "windwp/nvim-ts-autotag",
+  commit = "a1d526a",
+  opts = {
+    opts = {
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = true,
+    },
+  },
+}
