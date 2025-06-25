763a402f4f96d7ca2a2e6e3c6ebe1ece02d3cbbc
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-06-25 13:51:57 +0700

    chore(neovim): add keymaps for navigating windows

--- neovim/lua/config/keymaps.lua
+++ neovim/lua/config/keymaps.lua
@@ -2,3 +2,8 @@ local set = vim.keymap.set
 
 set({ "i", "n", "x", "s" }, "<c-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
 set("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Delete Buffer" })
+
+set({ "i", "n", "t" }, "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Go to Left Window", remap = false })
+set({ "i", "n", "t" }, "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Go to Bottom Window", remap = false })
+set({ "i", "n", "t" }, "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Go to Top Window", remap = false })
+set({ "i", "n", "t" }, "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Go to Right Window", remap = false })
