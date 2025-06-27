7a2a65caa0000705564f534f13e6bddfbe9aec8c
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-06-27 13:33:30 +0700

    chore(neovim): update neovim to version v0.11.2

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -4,6 +4,7 @@ vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 
 vim.diagnostic.config({
   float = { border = "rounded" },
+  virtual_text = true,
 })
 
 vim
@@ -17,6 +18,28 @@ vim
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
   end)
+
+local function get_lsp_filenames()
+  -- Find all .lua files in the identified 'lsp' directories
+  local lsp_dir = vim.fn.stdpath("config") .. "/lsp"
+  local lsp_files = vim.fs.find(function(name)
+    return name:match(".*%.lua$")
+  end, {
+    path = lsp_dir,
+    type = "file",
+    limit = math.huge, -- No limit on number of files
+  })
+
+  local filenames = {}
+  -- Extract the filename without the extension
+  for _, file_path in ipairs(lsp_files) do
+    table.insert(filenames, vim.fn.fnamemodify(file_path, ":t:r"))
+  end
+
+  return filenames
+end
+
+vim.lsp.enable(get_lsp_filenames())
 
 local opt = vim.opt
 
