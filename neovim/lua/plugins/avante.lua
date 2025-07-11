cbf90f867419fb13b0afca9604af2b87e108ded2
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-07-11 17:17:11 +0700

    chore(neovim/avante): update avante to version 0.0.25

--- neovim/lua/plugins/avante.lua
+++ neovim/lua/plugins/avante.lua
@@ -1,6 +1,6 @@
 return {
   "yetone/avante.nvim",
-  version = "0.0.23",
+  version = "0.0.25",
   event = "VeryLazy",
   lazy = true,
   dependencies = {
@@ -29,10 +29,12 @@ return {
   },
   opts = {
     provider = "gemini",
-    gemini = {
-      api_key_name = "GEMINI_API_KEY",
-      endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-      model = "gemini-2.5-flash-preview-05-20",
+    providers = {
+      gemini = {
+        api_key_name = "GEMINI_API_KEY",
+        endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+        model = "gemini-2.5-flash-preview-05-20",
+      },
     },
     windows = { width = 30 },
   },
