e2bf6a4ef850a0ea64ece74b820216f16b244f1c
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-08-05 21:54:39 +0700

    feat: add vim-tmux-navigator plugin

---
+++ neovim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,11 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  commit = "c45243d",
+  keys = {
+    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+  },
+}
