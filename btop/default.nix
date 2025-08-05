5324fa204a44b9d1547f00992cdfc6ca6eae6deb
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-08-06 06:16:41 +0700

    chore(btop): use home-manager options for configurations

--- btop/default.nix
+++ btop/default.nix
@@ -7,11 +7,13 @@ let
     rev = "1.0.0";
     sha256 = "J3UezOQMDdxpflGax0rGBF/XMiKqdqZXuX4KMVGTxFk=";
   };
-in {
-  home.packages = [pkgs.btop];
-
-  xdg.configFile = {
-    "btop/themes/catppuccin_latte.theme".source = catppuccin + "/themes/catppuccin_latte.theme";
-    "btop/themes/catppuccin_macchiato.theme".source = catppuccin + "/themes/catppuccin_macchiato.theme";
+in
+{
+  programs.btop = {
+    enable = true;
+    themes = {
+      catppuccin_latte = builtins.readFile (catppuccin + "/themes/catppuccin_latte.theme");
+      catppuccin_macchiato = builtins.readFile (catppuccin + "/themes/catppuccin_macchiato.theme");
+    };
   };
 }
