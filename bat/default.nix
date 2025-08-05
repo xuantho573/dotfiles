a4036181bfbc59610ab42622fab62f903d48d66b
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-08-06 06:06:20 +0700

    chore(bat): format config file

--- bat/default.nix
+++ bat/default.nix
@@ -3,22 +3,24 @@
 {
   programs.bat = {
     enable = true;
-    themes = let
-      catppuccin = pkgs.fetchFromGitHub {
-        owner = "catppuccin";
-        repo = "bat";
-        rev = "699f60fc8ec434574ca7451b444b880430319941";
-        sha256 = "6fWoCH90IGumAMc4buLRWL0N61op+AuMNN9CAR9/OdI=";
+    themes =
+      let
+        catppuccin = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "bat";
+          rev = "699f60fc8ec434574ca7451b444b880430319941";
+          sha256 = "6fWoCH90IGumAMc4buLRWL0N61op+AuMNN9CAR9/OdI=";
+        };
+      in
+      {
+        catppuccin-latte = {
+          src = catppuccin;
+          file = "themes/Catppuccin Latte.tmTheme";
+        };
+        catppuccin-macchiato = {
+          src = catppuccin;
+          file = "themes/Catppuccin Macchiato.tmTheme";
+        };
       };
-    in {
-      catppuccin-latte = {
-        src = catppuccin;
-        file = "themes/Catppuccin Latte.tmTheme";
-      };
-      catppuccin-macchiato = {
-        src = catppuccin;
-        file = "themes/Catppuccin Macchiato.tmTheme";
-      };
-    };
   };
 }
