2c1ff2cc0953fb17e01e3c0209a12c7dd0e0509e
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-08-06 09:01:58 +0700

    chore(neovim/lazydev): add libraries for lua lsp

--- neovim/lua/plugins/lazydev.lua
+++ neovim/lua/plugins/lazydev.lua
@@ -2,4 +2,11 @@ return {
   "folke/lazydev.nvim",
   ft = "lua",
   version = "1.9.0",
+  opts = {
+    library = {
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      { path = "snacks.nvim", words = { "Snacks" } },
+      { path = "lazy.nvim", words = { "LazyVim" } },
+    },
+  },
 }
