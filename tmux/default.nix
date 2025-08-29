6787fbd0ea372c3a7cc3d0073cb1b39f265e8728
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-08-29 07:00:13 +0700

    chore(home-manager): simplify tmux config file symlinking process

--- tmux/default.nix
+++ tmux/default.nix
@@ -8,23 +8,17 @@
 
   xdg.configFile = {
     "tmux/tmux.conf".source = mkSymlink ./tmux.conf;
-    "tmux/plugins/tpm" = {
-      recursive = true;
-      source = pkgs.fetchFromGitHub {
-        owner = "tmux-plugins";
-        repo = "tpm";
-        rev = "v3.1.0";
-        sha256 = "CeI9Wq6tHqV68woE11lIY4cLoNY8XWyXyMHTDmFKJKI=";
-      };
+    "tmux/plugins/tpm".source = pkgs.fetchFromGitHub {
+      owner = "tmux-plugins";
+      repo = "tpm";
+      rev = "v3.1.0";
+      sha256 = "CeI9Wq6tHqV68woE11lIY4cLoNY8XWyXyMHTDmFKJKI=";
     };
-    "tmux/plugins/catppuccin" = {
-      recursive = true;
-      source = pkgs.fetchFromGitHub {
-        owner = "catppuccin";
-        repo = "tmux";
-        rev = "v2.1.2";
-        sha256 = "vBYBvZrMGLpMU059a+Z4SEekWdQD0GrDqBQyqfkEHPg=";
-      };
+    "tmux/plugins/catppuccin".source = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "tmux";
+      rev = "v2.1.2";
+      sha256 = "vBYBvZrMGLpMU059a+Z4SEekWdQD0GrDqBQyqfkEHPg=";
     };
   };
 }
