2339242cde5f3ebc0be4a8a053900ee788c23d1d
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-09-11 18:50:30 +0700

    chore(neovim/diffview): add keymaps for diffview.nvim

--- neovim/lua/plugins/diffview.lua
+++ neovim/lua/plugins/diffview.lua
@@ -1,5 +1,13 @@
 return {
   "sindrets/diffview.nvim",
   commit = "4516612",
-  opts = {},
+  keys = {
+    { "<leader>dq", "<cmd>DiffviewClose<cr>", desc = "Quit Diff View" },
+    { "<leader>dm", "<cmd>DiffviewOpen origin/HEAD..HEAD<cr>", desc = "Compare with Main Branch" },
+  },
+  opts = {
+    default_args = {
+      DiffviewOpen = { "--imply-local" },
+    },
+  },
 }
