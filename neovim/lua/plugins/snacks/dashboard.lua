7fec9c484f7048a844a1791c89806566c602f7f5
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-09-16 14:11:47 +0700

    chore(neovim/snacks/dashboard): update session keymap for selecting session with mini.sessions

--- neovim/lua/plugins/snacks/dashboard.lua
+++ neovim/lua/plugins/snacks/dashboard.lua
@@ -15,8 +15,13 @@ return {
         { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
         { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
         { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-        { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', { cwd = vim.fn.stdpath('config') })" },
-        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+        {
+          icon = " ",
+          key = "c",
+          desc = "Config",
+          action = ":lua Snacks.dashboard.pick('files', { cwd = vim.fn.stdpath('config') })",
+        },
+        { icon = " ", key = "s", desc = "Restore Session", action = ":lua MiniSessions.select('read')" },
         { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
         { icon = " ", key = "q", desc = "Quit", action = ":qa" },
       },
@@ -26,5 +31,5 @@ return {
       { section = "keys", gap = 1, padding = 1 },
       { section = "startup" },
     },
-  }
+  },
 }
