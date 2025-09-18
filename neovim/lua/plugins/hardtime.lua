c466ffc2d01b8aaf2bca9a4f184934610aeedc82
Tho Nguyen Xuan <xuantho573@gmail.com>
2025-09-18 21:40:56 +0700

    chore(neovim): set up snacks.toggle for toggling hardtime.nvim

--- neovim/lua/plugins/hardtime.lua
+++ neovim/lua/plugins/hardtime.lua
@@ -1,9 +1,25 @@
 return {
-  "m4xshen/hardtime.nvim",
-  version = "1.2.0",
-  lazy = false,
-  dependencies = "MunifTanjim/nui.nvim",
-  opts = {
-    disable_mouse = false,
+  {
+    "m4xshen/hardtime.nvim",
+    version = "1.2.0",
+    lazy = false,
+    dependencies = "MunifTanjim/nui.nvim",
+    opts = {
+      disable_mouse = false,
+    },
+  },
+  {
+    "hardtime.nvim",
+    opts = function()
+      Snacks.toggle({
+        name = "Hardtime",
+        get = function()
+          return require("hardtime").is_plugin_enabled
+        end,
+        set = function()
+          vim.cmd("Hardtime toggle")
+        end,
+      }):map("<leader>th")
+    end,
   },
 }
