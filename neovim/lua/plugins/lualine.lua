7216c8577b13d4eca14135efbb636156004c8d54
Tho Nguyen Xuan <xuantho573@gmail.com>
2026-02-16 22:20:39 +0700

    chore(neovim/lualine.nvim): set filename component to display relative filepath

--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -26,7 +26,7 @@ return {
     sections = {
       lualine_a = { { "mode", separator = { left = "" } } },
       lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { { "filename", section_separators = { left = "_", right = "." } } },
+      lualine_c = { { "filename", path = 1, section_separators = { left = "_", right = "." } } },
       lualine_x = { "encoding", "fileformat", "filetype" },
       lualine_y = { "progress" },
       lualine_z = { { "location", separator = { right = "" } } },
