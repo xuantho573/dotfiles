a7d08ca04d9241f7d075e5f8ce95efda1428c063
Tho Nguyen Xuan <xuantho573@gmail.com>
2026-02-16 22:04:13 +0700

    chore(neovim/diffview.nvim): add command to open diffview

--- neovim/lua/plugins/diffview.lua
+++ neovim/lua/plugins/diffview.lua
@@ -3,6 +3,7 @@ return {
   commit = "4516612",
   keys = {
     { "<leader>dq", "<cmd>DiffviewClose<cr>", desc = "Quit Diff View" },
+    { "<leader>do", "<cmd>DiffviewOpen<cr>", desc = "Open Diff View" },
     { "<leader>dm", "<cmd>DiffviewOpen origin/HEAD..HEAD<cr>", desc = "Compare with Main Branch" },
     {
       "<leader>dt",
